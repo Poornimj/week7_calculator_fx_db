c90e8f238f8544a78bab252bed50ca1c6399a7c2
Poornimj <poornimj@metropolia.fi>
2026-03-05 15:23:38 +0200

    Week7: subtract and division with DB save + Jenkins

--- src/main/java/CalculatorController.java
+++ src/main/java/CalculatorController.java
@@ -1,4 +1,3 @@
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -12,19 +11,37 @@ public class CalculatorController {
     @FXML
     private void onCalculateClick() {
         try {
-            double num1 = Double.parseDouble(number1Field.getText());
-            double num2 = Double.parseDouble(number2Field.getText());
+            double num1 = Double.parseDouble(number1Field.getText().trim());
+            double num2 = Double.parseDouble(number2Field.getText().trim());
 
             double sum = num1 + num2;
             double product = num1 * num2;
+            double subtract = num1 - num2;
 
-            resultLabel.setText("Sum: " + sum + ", Product: " + product);
+            Double division = null;
+            String divisionText;
+            if (num2 == 0) {
+                divisionText = "Cannot divide by zero";
+            } else {
+                division = num1 / num2;
+                divisionText = String.valueOf(division);
+            }
+
+            resultLabel.setText(
+                    "Sum: " + sum +
+                            "\nProduct: " + product +
+                            "\nSubtract: " + subtract +
+                            "\nDivision: " + divisionText
+            );
 
             // Save to DB
-            ResultService.saveResult(num1, num2, sum, product);
+            ResultService.saveResult(num1, num2, sum, product, subtract, division);
 
         } catch (NumberFormatException e) {
             resultLabel.setText("Please enter valid numbers!");
+        } catch (Exception e) {
+            resultLabel.setText("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
